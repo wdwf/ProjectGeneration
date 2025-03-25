0c9456f8157799f84eb83c3f44928d1173ccc669
wdwf <w_dwf@hotmail.com>
2025-03-25 12:58:13 -0300

    :white_check_mark: adiciona classes para manipulação de Fila e Pilha em Java 9

---
+++ src/Java_9/Atividade1Fila.java
@@ -0,0 +1,80 @@
+package Java_9;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Atividade1Fila {
+    public static void main(String[] args) {
+        Queue<String> fila = new LinkedList<String>();
+        int opcao;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("********************************");
+            System.out.println("1 - Adicionar Cliente na fila");
+            System.out.println("2 - Listar todos os Clientes");
+            System.out.println("3 - Retirar Cliente da fila");
+            System.out.println("0 - Sair");
+            System.out.println("********************************");
+            opcao = lerNumero(input);
+            input.skip("\\R?");
+
+            if (opcao == 0) {
+                System.out.println("Programa Finalizado!");
+                break;
+            }
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome do cliente: ");
+                    fila.add(input.nextLine());
+                    System.out.println("\nFila:\n");
+                    listarClientes(fila);
+                    System.out.println("\nCliente Adicionado!\n");
+                    break;
+                case 2:
+                    System.out.println("\nLista de Clientes na Fila:\n");
+                    listarClientes(fila);
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    if (fila.isEmpty()) {
+                        System.out.println("\nA Fila está vazia.\n");
+                        break;
+                    }
+                    fila.remove();
+                    System.out.println("\nFila:\n");
+                    listarClientes(fila);
+                    System.out.println("\nO Cliente foi Chamado!\n");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+
+        input.close();
+    }
+
+    public static void listarClientes(Queue<String> fila) {
+        Iterator<String> iterator = fila.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static int lerNumero(Scanner input) {
+        while (true) {
+            try {
+                System.out.print("Entre com a opção desejada: ");
+                return input.nextInt();
+            } catch (Exception e) {
+                System.out.println("\nEntrada inválida. Digite uma opção de acordo com o menu.\n");
+                input.nextLine();
+            }
+        }
+    }
+}
---
+++ src/Java_9/Atividade2Pilha.java
@@ -0,0 +1,79 @@
+package Java_9;
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Atividade2Pilha {
+    public static void main(String[] args) {
+        Stack<String> pilha = new Stack<String>();
+        int opcao;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("********************************");
+            System.out.println("1 - Adicionar Livro na pilha");
+            System.out.println("2 - Listar todos os Livros");
+            System.out.println("3 - Retirar Livro da pilha");
+            System.out.println("0 - Sair");
+            System.out.println("********************************");
+            opcao = validaOpcao(input);
+            input.skip("\\R?");
+
+            if (opcao == 0) {
+                System.out.println("Programa Finalizado!");
+                break;
+            }
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o titulo do livro: ");
+                    pilha.push(input.nextLine());
+                    System.out.println("\nPilha:\n");
+                    listarLivros(pilha);
+                    System.out.println("\nLivro Adicionado!\n");
+                    break;
+                case 2:
+                    System.out.println("\nLista de Livros na Pilha:\n");
+                    listarLivros(pilha);
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    if (pilha.isEmpty()) {
+                        System.out.println("\nA Pilha está vazia.\n");
+                        break;
+                    }
+                    pilha.pop();
+                    System.out.println("\nPilha:\n");
+                    listarLivros(pilha);
+                    System.out.println("\nO Livro foi retirado da pilha!\n");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+
+        input.close();
+    }
+
+    public static void listarLivros(Stack<String> pilha) {
+        Iterator<String> iterator = pilha.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static int validaOpcao(Scanner input) {
+        while (true) {
+            try {
+                System.out.print("Entre com a opção desejada: ");
+                return input.nextInt();
+            } catch (Exception e) {
+                System.out.println("\nEntrada inválida. Digite uma opção de acordo com o menu.\n");
+                input.nextLine();
+            }
+        }
+    }
+}
